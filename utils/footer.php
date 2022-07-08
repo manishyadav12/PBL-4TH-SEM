3fa7ec5969e3ee648576218d166b8a784e87d708
Manish <manish25102@gmail.com>
2022-07-08 22:44:24 +0530

    EMS

---
+++ utils/footer.php
@@ -0,0 +1,43 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>B</strong><span class = "small footerSubtext"></span>
+                        <strong>M</strong><span class = "small footerSubtext"></span>
+                        <strong>S</strong><span class = "small footerSubtext"></span>
+                        <strong>I</strong><span class = "small footerSubtext">nstitue of</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology and</span>
+                        <strong>M</strong><span class = "small footerSubtext">anagement</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Our college Mission is to impart quality technical education and higher moral ethics associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future with full confidence
+                  .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address: Doddaballapur Main Road,
+                            Avalahalli, Yelahanka,
+                            
+
+                    <br>Bengaluru, Karnataka 560064 </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                    <a href="https://www.facebook.com/bmsit"><img src = "images/facebook.png"></a>
+                    <a href="https://www.instagram.com/bmsit_m/?hl=en"><img src = "images/instagram.png" width="65px"></a>
+                        
+                        <a href="https://www.youtube.com/channel/UCZLUwjJYZZvXvfPketkZZMQ"><img src = "images/youtube.png"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
